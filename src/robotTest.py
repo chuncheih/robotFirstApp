714016cffa4e8c824a452663332741d58c2d2367
chuncheih <scout.chen@gmail.com>
2026-02-20 22:03:46 +0800

    Commit First app for robot

---
+++ src/robotTest.py
@@ -0,0 +1,9 @@
+from reachy_mini import ReachyMini
+from reachy_mini.utils import create_head_pose
+
+with ReachyMini() as mini:
+    # Look up and tilt head
+    mini.goto_target(
+        head=create_head_pose(z=10, roll=15, degrees=True, mm=True),
+        duration=1.0
+    )
